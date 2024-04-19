9604332040201a4a1472d481c31d77afd77c166b
rudyP123 <ruduspatel@gmail.com>
2024-04-19 14:25:45 -0400

    updated slides to personal google account

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Presentation() {
-  const presentationUrl = "https://docs.google.com/presentation/d/16o91oekJ8TTabjp-rzYJj-MFAdghXSg_QmQiOCdeVZI/edit?usp=sharing/embed?start=false&loop=false&delayms=3000";
+  const presentationUrl = "https://docs.google.com/presentation/d/1J9pd4Ov8hJ_X2r6jdpCf6cr0BKiGLKa4bgC2ABz8GIc/edit?usp=sharing/edit?usp=sharing/embed?start=false&loop=false&delayms=3000";
   return (
     <div>
       <h1>Presentation Slides</h1>
